ca500a4bda828a509d3f095581eaeba41f4250ff
wb1110 <strengthcoachwb@gmail.com>
2022-01-11 16:44:05 -0600

    put done

--- api/server.js
+++ api/server.js
@@ -55,15 +55,39 @@ server.post('/api/posts', (req, res) => {
 })
 
 server.put('/api/posts/:id', (req, res) => {
-    const { id } = req.params
-    const post = req.body
-    Posts.update(id, post)
-    .then ((updatedPost) => {
-        res.status(200).json(updatedPost)
-    })
-    .catch (err => {
-        res.status(500).json({ message: "The post information could not be modified" })
-    })
+    const { title, contents } = req.body
+    if (!title || !contents) {
+        res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    } else {
+        Posts.findById(req.params.id)
+        .then(stuff => {
+            if (!stuff) {
+                res.status(404).json({ 
+                    message: "The post with the specified ID does not exist",
+                }) 
+            } else { 
+                return Posts.update(req.params.id, req.body)
+            }
+        })
+        .then(data => {
+            if (data) {
+                return Posts.findById(req.params.id)
+            }
+        })
+        .then(post => {
+            if (post) {
+                res.json(post)
+            }
+        })
+        .catch (err => {
+            res.status(500).json({ message: "The post information could not be modified" })
+        })
+        
+
+    }
+    
 })
 
 server.delete('/api/posts/:id', (req, res) => {
